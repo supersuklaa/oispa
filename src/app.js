eb0a9ffa03730ab8e5d0a6af405a7357296f64e3
supersuklaa <antti.sartjarvi@gmail.com>
2020-10-07 00:55:27 +0300

    Add proper linters

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import { h, text, app } from 'hyperapp';
-import { main, div, button, img } from '@hyperapp/html';
+import {
+  main, div, button, img,
+} from '@hyperapp/html';
 
 import doge from '../static/kaljadoge.jpg';
 
@@ -12,34 +14,28 @@ const Spin = (dispatch, stop) => {
   }, 300);
 };
 
-const Stop = state => ({ ...state, spin: '' });
+const Stop = (state) => ({ ...state, spin: '' });
 
-const Subtract = state => [{ spin: 'left', zoom: Math.max(state.zoom - 1, minZoom) },
-  [
-    Spin, Stop,
-  ],
+const Subtract = (state) => [
+  { spin: 'left', zoom: Math.max(state.zoom - 1, minZoom) },
+  [Spin, Stop],
 ];
-const Add = state => [{ spin: 'right', zoom: Math.min(state.zoom + 1, maxZoom) },
-  [
-    Spin, Stop,
-  ],
+
+const Add = (state) => [
+  { spin: 'right', zoom: Math.min(state.zoom + 1, maxZoom) },
+  [Spin, Stop],
 ];
 
 app({
   init: (zoom = 0, spin = '') => ({ zoom, spin }),
-  view: ({ zoom, spin }) =>
-    main({ class: 'app' }, [
-      div({ class: 'nabuls' }, [
-        button(
-          { onclick: Subtract, disabled: zoom === minZoom },
-          text('-'),
-        ),
-        button(
-          { onclick: Add, disabled: zoom === maxZoom },
-          text('+'),
-        ),
-      ]),
-      div({ class: 'doggelis' }, [img({ src: doge, class: `zoom-${zoom} spin-${spin}` })]),
+  view: ({ zoom, spin }) => main({ class: 'app' }, [
+    div({ class: 'nabuls' }, [
+      button({ onclick: Subtract, disabled: zoom === minZoom }, text('-')),
+      button({ onclick: Add, disabled: zoom === maxZoom }, text('+')),
     ]),
+    div({ class: 'doggelis' }, [
+      img({ src: doge, class: `zoom-${zoom} spin-${spin}` }),
+    ]),
+  ]),
   node: document.getElementById('app'),
 });
